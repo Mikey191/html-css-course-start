9dd47eb470155b773b7a7e230a497f65e8e58555
Mikey191 <desertigorb@gmail.com>
2025-01-20 18:07:33 +0300

    js 3 4

--- app.js
+++ app.js
@@ -1,17 +1,1 @@
-let fund = +prompt("Credit sum");
-let creditBody = fund + 0.2 * fund;
-let monthlyPayment = creditBody / 12;
 
-for (let month = 1; month <= 12; month++) {
-  console.log("month " + month + " payment " + monthlyPayment);
-}
-
-let arr = [1, 2, 3, 4, 5, 6];
-
-for (let element in arr) {
-  console.log(element);
-}
-
-for (let element of arr) {
-  console.log(element);
-}
--- js_practice/index.js
+++ js_practice/index.js
@@ -1,8 +1,11 @@
-for (let i = 1; i <= N; i++) {
-  // Создаем элемент h2
-  const header = document.createElement("h2");
-  // Устанавливаем текст заголовка
-  header.textContent = `Header${i}`;
-  // Добавляем заголовок на страницу
-  document.body.appendChild(header);
+function bankerRound(x){
+  if (Math.round(x) % 2 == 0) return Math.round(x);
+  else {
+    if (x < Math.round(x)) return Math.round(x) - 1;
+    else return Math.round(x) + 1;
+  }
 }
+
+console.log(bankerRound(2.7)); // 2
+console.log(bankerRound(3.2)); // 4
+console.log(bankerRound(4.3)); // 4
